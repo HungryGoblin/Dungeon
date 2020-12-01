e6f16f4a1447ada230a3d1bdd5b5f801724c374c
Andrey <sozykin@yandex.ru>
2020-12-01 19:42:45 +0300

    Lesson 7

--- core/src/ru/geekbrains/dungeon/game/GameMap.java
+++ core/src/ru/geekbrains/dungeon/game/GameMap.java
@@ -3,20 +3,40 @@ package ru.geekbrains.dungeon.game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
+import lombok.Data;
+import ru.geekbrains.dungeon.game.units.Unit;
 import ru.geekbrains.dungeon.helpers.Assets;
 
 public class GameMap {
+
     public enum CellType {
-        GRASS, WATER, TREE
+        WATER(0), GRASS(1), TREE(0), MOOD(2);
+        int overCost;
+
+        CellType(int overCost) {
+            overCost = overCost;
+        }
     }
 
-    private class Cell {
+    public enum DropType {
+        NONE, GOLD
+    }
+
+    public class Cell {
         CellType type;
+        DropType dropType;
+
+        int dropPower;
         int index;
 
         public Cell() {
             type = CellType.GRASS;
+            dropType = DropType.NONE;
             index = 0;
+        }
+
+        public int getCellOverCost() {
+            return type.overCost;
         }
 
         public void changeType(CellType to) {
@@ -31,6 +51,7 @@ public class GameMap {
     public static final int CELLS_Y = 12;
     public static final int CELL_SIZE = 60;
     public static final int FOREST_PERCENTAGE = 5;
+    public static final int MOOD_PERCENTAGE = 10;
 
     public int getCellsX() {
         return CELLS_X;
@@ -42,6 +63,8 @@ public class GameMap {
 
     private Cell[][] data;
     private TextureRegion grassTexture;
+    private TextureRegion moodTexture;
+    private TextureRegion goldTexture;
     private TextureRegion[] treesTextures;
 
     public GameMap() {
@@ -51,13 +74,18 @@ public class GameMap {
                 this.data[i][j] = new Cell();
             }
         }
-        int treesCount = (int)((CELLS_X * CELLS_Y * FOREST_PERCENTAGE) / 100.0f);
+        int treesCount = (int) ((CELLS_X * CELLS_Y * FOREST_PERCENTAGE) / 100.0f);
         for (int i = 0; i < treesCount; i++) {
             this.data[MathUtils.random(0, CELLS_X - 1)][MathUtils.random(0, CELLS_Y - 1)].changeType(CellType.TREE);
-
+        }
+        int moodCount = (int) ((CELLS_X * CELLS_Y * MOOD_PERCENTAGE) / 100.0f);
+        for (int i = 0; i < moodCount; i++) {
+            this.data[MathUtils.random(0, CELLS_X - 1)][MathUtils.random(0, CELLS_Y - 1)].changeType(CellType.MOOD);
         }
 
         this.grassTexture = Assets.getInstance().getAtlas().findRegion("grass");
+        this.moodTexture = Assets.getInstance().getAtlas().findRegion("grass");
+        this.goldTexture = Assets.getInstance().getAtlas().findRegion("chest").split(60, 60)[0][0];
         this.treesTextures = Assets.getInstance().getAtlas().findRegion("trees").split(60, 90)[0];
     }
 
@@ -65,20 +93,61 @@ public class GameMap {
         if (cx < 0 || cx > getCellsX() - 1 || cy < 0 || cy > getCellsY() - 1) {
             return false;
         }
-        if (data[cx][cy].type != CellType.GRASS) {
+        if (data[cx][cy].type != CellType.GRASS || data[cx][cy].type != CellType.MOOD) {
             return false;
         }
         return true;
+    }
+
+    public Cell getCell(int cx, int cy) {
+        return data[cx][cy];
     }
 
     public void render(SpriteBatch batch) {
         for (int i = 0; i < CELLS_X; i++) {
             for (int j = CELLS_Y - 1; j >= 0; j--) {
                 batch.draw(grassTexture, i * CELL_SIZE, j * CELL_SIZE);
+                if (data[i][j].type == CellType.MOOD) { ;
+                    batch.setColor(0.7f, 0.7f, 0.7f, 1);
+                    batch.draw(moodTexture, i * CELL_SIZE, j * CELL_SIZE);
+                    batch.setColor(1f, 1f, 1f, 1);
+                }
                 if (data[i][j].type == CellType.TREE) {
                     batch.draw(treesTextures[data[i][j].index], i * CELL_SIZE, j * CELL_SIZE);
                 }
+                if (data[i][j].dropType == DropType.GOLD) {
+                    batch.draw(goldTexture, i * CELL_SIZE, j * CELL_SIZE);
+                }
             }
         }
+    }
+
+    // todo: перенести в калькулятор
+    public void generateDrop(int cellX, int cellY, int power) {
+        if (MathUtils.random() < 0.5f) {
+            DropType randomDropType = DropType.GOLD;
+
+            if (randomDropType == DropType.GOLD) {
+                int goldAmount = power + MathUtils.random(power, power * 3);
+                data[cellX][cellY].dropType = randomDropType;
+                data[cellX][cellY].dropPower = goldAmount;
+            }
+        }
+    }
+
+    public boolean hasDropInCell(int cellX, int cellY) {
+        return data[cellX][cellY].dropType != DropType.NONE;
+    }
+
+    public void checkAndTakeDrop(Unit unit) {
+        Cell currentCell = data[unit.getCellX()][unit.getCellY()];
+        if (currentCell.dropType == DropType.NONE) {
+            return;
+        }
+        if (currentCell.dropType == DropType.GOLD) {
+            unit.addGold(currentCell.dropPower);
+        }
+        currentCell.dropType = DropType.NONE;
+        currentCell.dropPower = 0;
     }
 }
--- core/src/ru/geekbrains/dungeon/screens/ScreenManager.java
+++ core/src/ru/geekbrains/dungeon/screens/ScreenManager.java
@@ -1,16 +1,15 @@
 package ru.geekbrains.dungeon.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.*;
-import lombok.Data;
 import ru.geekbrains.dungeon.DungeonGame;
 import ru.geekbrains.dungeon.helpers.Assets;
 
-@Data
 public class ScreenManager {
     public enum ScreenType {
         MENU, GAME
@@ -26,6 +25,7 @@ public class ScreenManager {
 
     private LoadingScreen loadingScreen;
     private GameScreen gameScreen;
+    private MenuScreen menuScreen;
 
     private Screen targetScreen;
     private Viewport viewport;
@@ -54,6 +54,7 @@ public class ScreenManager {
         this.camera = new OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT);
         this.viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
         this.gameScreen = new GameScreen(batch);
+        this.menuScreen = new MenuScreen(batch);
         this.loadingScreen = new LoadingScreen(batch);
     }
 
@@ -81,8 +82,13 @@ public class ScreenManager {
             screen.dispose();
         }
         resetCamera();
+        Gdx.input.setInputProcessor(null);
         game.setScreen(loadingScreen);
         switch (type) {
+            case MENU:
+                targetScreen = menuScreen;
+                Assets.getInstance().loadAssets(ScreenType.MENU);
+                break;
             case GAME:
                 targetScreen = gameScreen;
                 Assets.getInstance().loadAssets(ScreenType.GAME);
